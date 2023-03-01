7b796d12b683259deba7eaac4663eac58ff23d7b
OrangeBurrito <root@orangeburrito.com>
2023-03-01 23:38:09 +0700

    initial

---
+++ src/main/java/com/orangeburrito/scenic/Scenic.java
@@ -0,0 +1,26 @@
+package com.orangeburrito.scenic;
+
+import com.mojang.logging.LogUtils;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(Scenic.MODID)
+public class Scenic {
+	private static final Logger LOGGER = LogUtils.getLogger();
+	public static final String MODID = "scenic";
+
+	public Scenic() {
+		final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.addListener(this::setup);
+
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	private void setup(final FMLCommonSetupEvent event) {
+	}
+}
